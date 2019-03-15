f52c1adcd3a8e0950e5f3ae6e498995323bc820c
Andrew Shitov <andy@shitov.ru>
2019-03-15 09:33:00 +0100

    install

---
+++ apt-install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+apt update
+
+apt install mc
+apt install nginx
+apt install build-essential
+apt install libdbi-perl
+apt install libxml-libxml-perl
+apt install libxml-libxslt-perl
+apt install starman
+apt install mysql-server
+apt install mysql-client libmysqlclient-dev
+apt install libdbd-mysql-perl
